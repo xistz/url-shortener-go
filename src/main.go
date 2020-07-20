3934134c600925809949947512c16d00316b187d
Jonathan Chan <tmxistz@gmail.com>
2020-07-20 17:53:27 +0900

    created a simple server

--- src/main.go
+++ src/main.go
@@ -1,5 +1,32 @@
 package main
 
-func sayHello() string {
-	return "hello"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+)
+
+// Message struct
+type Message struct {
+	Message string `json:"message"`
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	message := Message{"hello"}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(message)
+}
+
+func main() {
+	port, ok := os.LookupEnv("PORT")
+	if !ok {
+		port = "3000"
+	}
+
+	http.HandleFunc("/", rootHandler)
+
+	log.Println("Listenining on port: " + port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
--- src/main_test.go
+++ src/main_test.go
@@ -1,11 +1,46 @@
 package main
 
-import "testing"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
 
-func TestSayHello(t *testing.T) {
-	got := sayHello()
-
-	if expected := "hello"; got != expected {
-		t.Errorf("expected: %s; got: %s", expected, got)
+func TestRootHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatalf("could not create request: %v", err)
 	}
+
+	rec := httptest.NewRecorder()
+	rootHandler(rec, req)
+
+	res := rec.Result()
+	defer res.Body.Close()
+
+	t.Run(fmt.Sprintf("returns %v", http.StatusOK), func(t *testing.T) {
+		if res.StatusCode != http.StatusOK {
+			t.Errorf("Expected: %v; Got: %v", http.StatusOK, res.StatusCode)
+		}
+	})
+
+	t.Run(fmt.Sprintf("returns '%s'", "application/json"), func(t *testing.T) {
+		if expected, got := "application/json", res.Header.Get("Content-Type"); got != expected {
+			t.Errorf("Expected: %v; Got: %v", expected, got)
+		}
+	})
+
+	t.Run("returns expected JSON", func(t *testing.T) {
+		expected := Message{"hello"}
+		var got Message
+		if err := json.NewDecoder(res.Body).Decode(&got); err != nil {
+			t.Fatalf("could not read json: %v", err)
+		}
+
+		if got.Message != expected.Message {
+			t.Errorf("Expected: {'message': '%s'}; Got: {'message': '%s'}", expected.Message, got.Message)
+		}
+	})
 }
